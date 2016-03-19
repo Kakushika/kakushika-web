776c6056e43e110ea5e97898023253c88df1b52f
Orleika <orleika.net@gmail.com>
2016-03-19 22:57:55 +0900

    Add gulp tasks

---
+++ gulpfile.babel.js
@@ -0,0 +1,82 @@
+/* eslint strict: 0, arrow-body-style: 0 */
+'use strict';
+
+import del from 'del';
+import runSequence from 'run-sequence';
+import gulp from 'gulp';
+import mainBowerFiles from 'main-bower-files';
+import gulpLoadPlugins from 'gulp-load-plugins';
+
+const $ = gulpLoadPlugins();
+
+gulp.task('clean', () => del(['dist/**/*.html', 'dist/**/*.css', 'dist/**/**.js', 'tmp/*.html']));
+
+gulp.task('deploy-lib', () => {
+  return gulp.src(mainBowerFiles())
+    .pipe(gulp.dest('dist/lib'));
+});
+
+gulp.task('style', () => {
+  return gulp.src('src/sass/*.scss')
+    .pipe($.plumber({
+      errorHandler: $.notify.onError('<%= error.message %>'),
+    }))
+    .pipe($.compass({
+      config_file: 'src/config.rb',
+      comments: false,
+      css: 'dist/css',
+      sass: 'src/sass',
+      image: 'dist/img',
+    }))
+    .pipe($.autoprefixer())
+    .pipe($.minifyCss())
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('script', () => {
+  return gulp.src('src/js/*.js')
+    .pipe($.plumber({
+      errorHandler: $.notify.onError('<%= error.message %>'),
+    }))
+    .pipe($.babel())
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('markup', () => {
+  return gulp.src('src/extend/views/**/*.html')
+    .pipe($.plumber({
+      errorHandler: $.notify.onError('<%= error.message %>'),
+    }))
+    .pipe($.htmlExtend())
+    .pipe(gulp.dest('tmp'));
+});
+
+gulp.task('minify-html', () => {
+  return gulp.src('tmp/*.html')
+    .pipe($.plumber({
+      errorHandler: $.notify.onError('<%= error.message %>'),
+    }))
+    .pipe($.minifyHtml({
+      empty: true,
+      cdata: true,
+      conditionals: true,
+      spare: true,
+      quotes: true,
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', () => {
+  gulp.watch('src/sass/*.scss', ['style']);
+  gulp.watch('src/js/*.js', ['script']);
+  gulp.watch('src/extend/**/*.html', ['markup']);
+  gulp.watch('tmp/*.html', ['minify-html']);
+});
+
+gulp.task('default', ['clean'], cb => {
+  runSequence(
+    ['deploy-lib', 'style', 'script', 'markup'],
+    ['minify-html'],
+    cb
+  );
+});
